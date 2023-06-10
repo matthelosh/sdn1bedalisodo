24e698fcacad6be30028c963140a127b62583e95
matthelosh@gmail.com <matthelosh@gmail.com>
2023-06-10 12:58:40 +0700

    tes front nav link

--- resources/js/Layouts/Navbar.vue
+++ resources/js/Layouts/Navbar.vue
@@ -44,7 +44,7 @@ const search = (e) => {
                             Home
                         </a>
                     </li>
-                    <li><Link :href="route('about')" class="block tracking-wide transition-all p-3 text-secondary hover:font-semibold">About</Link></li>
+                    <li><Link href="/about" class="block tracking-wide transition-all p-3 text-secondary hover:font-semibold">About</Link></li>
                     <li><a href="#" class="block tracking-wide transition-all p-3 text-secondary hover:font-semibold">Students</a></li>
                     <li>
                         <form @submit.prevent="search">
