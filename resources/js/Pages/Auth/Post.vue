e8575eef0931e31073e6da3e7d33b9978557d2bc
Muhammad Soleh <matthelosh@gmail.com>
2023-06-13 07:00:18 +0700

    Mulai ke Dashboard

---
+++ resources/js/Pages/Auth/Post.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+</script>
+
+<template>
+<Head title="Post" />   
+<AdminLayout title="Postingan">
+    <h1>Post</h1>
+</AdminLayout>
+</template>
