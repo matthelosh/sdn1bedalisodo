a7ea6aa9d01c07371c9fa58ce196ecfa0c9183da
Muhammad Soleh <matthelosh@gmail.com>
2023-08-17 13:49:57 +0700

    Add api routes for Waapi

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Sekolah;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get("/sekolah", function(Request $request) {
+    $token = $request->header('token');
+    // return response()->json(['status' => $token]);
+    $key = Hash::make('mandita');
+    if(Hash::check($token, $key)) {
+        $sekolah = Sekolah::first();
+        return response()->json(['status' => "OK", "sekolah" => $sekolah], 200);
+    } else {
+        return response()->json(['status' => 'Unauthorized'], 403);
+    }
+    
 });
