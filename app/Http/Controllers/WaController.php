b25622246ac694f77ef1e63f613ad646a3fae9dd
Muhammad Soleh <matthelosh@gmail.com>
2023-08-18 13:35:00 +0700

    Add guru api to waapi and nuptk to gurus

--- app/Http/Controllers/WaController.php
+++ app/Http/Controllers/WaController.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Guru;
+use App\Models\Siswa;
+use App\Models\Tapel;
+use App\Models\Rombel;
+use GuzzleHttp\Client;
+use App\Models\Sekolah;
+use App\Services\Waapi;
 use App\Traits\WablasTrait;
 use Illuminate\Http\Request;
-use App\Models\Guru;
-use GuzzleHttp\Client;
-use App\Services\Waapi;
 
 class WaController extends Controller
 {
 
     public function sendToUsers(Request $request) {
         $gurus = Guru::whereNot('role', 'admin')->get();
-        // dd($gurus);
         $message = json_decode($request->data);
         $datas = [];
         foreach($gurus as $guru)
@@ -40,7 +43,6 @@ class WaController extends Controller
 
     public function send(Request $request, Waapi $waapi)
     {
-        // dd(json_decode($request->data));
         try {
             $message = json_decode($request->data);
                 $params = [
@@ -70,7 +72,118 @@ class WaController extends Controller
         }
     }
     
-    function kirim($datas) {
-    } 
+    public function replyDataSekolah(Request $request) {
+        $sekolah = Sekolah::with('ks')->first();
+        return response()->json(['status' => 'ok', 'sekolah' => $sekolah], 200);
+    }
+
+    public function replyDataSiswa(Request $request) {
+        try {
+            $tapel = Tapel::where('status', '1')->first();
+            $kode_tapel = $tapel->kode;
+            $res = 'Berikut adalah info siswa yang dapat saya berikan:'."\n";
+    
+            if($request->query('kelas') && !$request->query('nama')) {
+                if ($request->query('kelas') !== 'all') {
+                    $rombels = Rombel::where('kode', 'LIKE', '%'.$request->query('kelas'))->where('tapel', $tapel->kode)->with('siswas','guru')->get();
+                } else {
+                    $rombels = Rombel::where('tapel', $tapel->kode)->with('siswas', 'guru')->get();
+                }
+                
+                foreach($rombels as $rombel)
+                {
+                    $label = $rombel->label;
+                    $jml = count($rombel->siswas);
+                    $lk = count($rombel->siswas->filter(function ($siswa) {
+                        return $siswa->jk == 'Laki-laki';
+                    })->values());
+                    $pr = count($rombel->siswas->filter(function ($siswa) {
+                        return $siswa->jk == 'Perempuan';
+                    })->values());
+                $res .= "
+    $label: 
+    Lk:  $lk, Pr: $pr, Jml: $jml\n";
+            }
+            
+                
+            }
+    
+            if ($request->query('nama') && !$request->query('kelas')) {
+                $siswas = Siswa::where('nama','LIKE', '%'.$request->query('nama').'%')
+                ->with('rombel')
+                ->get();
+                if(count($siswas) < 1) {
+                    $res = "Maaf, saya tidak menemukan siswa dengan nama:". $request->query('nama')."\n\nSilahkan menanyakan kepada operator Sekolah apakah sudah dimasukkan." ;
+                }
+                foreach($siswas as $siswa) {
+                    $rombel = count($siswa->rombel) > 0 ? $siswa->rombel[0]->label : '-';
+                    $res .= "
+    Nama: $siswa->nama,
+    NISN: $siswa->nisn,
+    NIS: $siswa->nis,
+    JK: $siswa->jk,
+    TTL: $siswa->tempat_lahir , $siswa->tanggal_lahir,
+    Alamat: $siswa->alamat,
+    No HP: $siswa->hp,
+    Kelas: $rombel,
+    Status: $siswa->status
+                    ";
+                }
+    
+            }
+    
+            if($request->query('kelas') && $request->query('name')) {
+                $rombel = $request->query('kelas');
+                $siswas = Siswa::where('nama','LIKE','%'.$request->query('nama').'%')
+                                ->whereHas('rombel', function($q) use($rombel) {
+                                    $q->where('rombels.kode', 'LIKE', '%'.$rombel);
+                                })->get();
+    
+                foreach($siswas as $siswa) {
+                    $res .= "
+    Nama: $siswa->nama,
+    NISN: $siswa->nisn,
+    NIS: $siswa->nis,
+    JK: $siswa->jk,
+    TTL: $siswa->tempat_lahir , $siswa->tanggal_lahir,
+    Alamat: $siswa->alamat,
+    No HP: $siswa->hp,
+    Kelas: $siswa->rombel->label
+    Status: $siswa->status
+                    ";
+                }
+    
+            }
+            return response()->json(['status' => 'ok', 'siswas' => $res], 200);
+        } catch(\Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    public function replyDataGuru(Request $request) {
+        $q = $request->query('nama') == 'all' ? '%' : $request->query('nama');
+
+        try {
+            $res = "Berikut Data Guru yang Anda Cari:\n";
+            $gurus = Guru::whereNot('role', 'admin')->where('nama', 'LIKE', '%'.$q.'%')->get();
+
+            foreach($gurus as $guru)
+            {
+                $res .= "
+Nama: $guru->nama,
+Gelar Belakang: $guru->gelar_belakang,
+NIP: $guru->nip,
+NUPTK: $guru->nuptk,
+Pangkat: $guru->pangkat,
+Alamat: $guru->alamat,
+HP: $guru->hp,
+==============================
+                ";
+            }
+            return response()->json(['status' => 'ok', 'gurus' => $res], 200);
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
+    }
     
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WaController;
 use App\Models\Sekolah;
 use App\Models\Tapel;
 use App\Models\Rombel;
@@ -23,94 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/sekolah", function(Request $request) {
-    $sekolah = Sekolah::with('ks')->first();
-    return response()->json(['status' => 'ok', 'sekolah' => $sekolah], 200);
-})->middleware('isLegal');
+Route::get("/sekolah", [WaController::class, 'replyDataSekolah'])->middleware('isLegal');
 
-Route::get('/siswa', function(Request $request) {
-    try {
-        $tapel = Tapel::where('status', '1')->first();
-        $kode_tapel = $tapel->kode;
-        $res = 'Berikut adalah info siswa yang dapat saya berikan:'."\n";
-
-        if($request->query('kelas') && !$request->query('nama')) {
-            if ($request->query('kelas') !== 'all') {
-                $rombels = Rombel::where('kode', 'LIKE', '%'.$request->query('kelas'))->where('tapel', $tapel->kode)->with('siswas','guru')->get();
-            } else {
-                $rombels = Rombel::where('tapel', $tapel->kode)->with('siswas', 'guru')->get();
-            }
-            
-            foreach($rombels as $rombel)
-            {
-                $label = $rombel->label;
-                $jml = count($rombel->siswas);
-                $lk = count($rombel->siswas->filter(function ($siswa) {
-                    return $siswa->jk == 'Laki-laki';
-                })->values());
-                $pr = count($rombel->siswas->filter(function ($siswa) {
-                    return $siswa->jk == 'Perempuan';
-                })->values());
-            $res .= "
-$label: 
-Lk:  $lk, Pr: $pr, Jml: $jml\n";
-        }
-        
-            
-        }
-
-        if ($request->query('nama') && !$request->query('kelas')) {
-            $siswas = Siswa::where('nama','LIKE', '%'.$request->query('nama').'%')
-            // ->whereHas('rombel', function($q) use($kode_tapel) {
-            //     $q->where('tapel', $kode_tapel);
-            // })
-            ->with('rombel')
-            ->get();
-            if(count($siswas) < 1) {
-                $res = "Maaf, saya tidak menemukan siswa dengan nama:". $request->query('nama')."\n\nSilahkan menanyakan kepada operator Sekolah apakah sudah dimasukkan." ;
-            }
-            // $res .= count($siswas)."\n";
-            foreach($siswas as $siswa) {
-                $rombel = count($siswa->rombel) > 0 ? $siswa->rombel[0]->label : '-';
-                $res .= "
-Nama: $siswa->nama,
-NISN: $siswa->nisn,
-NIS: $siswa->nis,
-JK: $siswa->jk,
-TTL: $siswa->tempat_lahir , $siswa->tanggal_lahir,
-Alamat: $siswa->alamat,
-No HP: $siswa->hp,
-Kelas: $rombel,
-Status: $siswa->status
-                ";
-            }
-
-        }
-
-        if($request->query('kelas') && $request->query('name')) {
-            $rombel = $request->query('kelas');
-            $siswas = Siswa::where('nama','LIKE','%'.$request->query('nama').'%')
-                            ->whereHas('rombel', function($q) use($rombel) {
-                                $q->where('rombels.kode', 'LIKE', '%'.$rombel);
-                            })->get();
-
-            foreach($siswas as $siswa) {
-                $res .= "
-Nama: $siswa->nama,
-NISN: $siswa->nisn,
-NIS: $siswa->nis,
-JK: $siswa->jk,
-TTL: $siswa->tempat_lahir , $siswa->tanggal_lahir,
-Alamat: $siswa->alamat,
-No HP: $siswa->hp,
-Kelas: $siswa->rombel->label
-Status: $siswa->status
-                ";
-            }
-
-        }
-        return response()->json(['status' => 'ok', 'siswas' => $res], 200);
-    } catch(\Exception $e) {
-        dd($e->getMessage());
-    }
-})->middleware('isLegal');
+Route::get('/siswa', [WaController::class, 'replyDataSiswa'])->middleware('isLegal');
+Route::get('/guru', [WaController::class, 'replyDataGuru'])->middleware('isLegal');
