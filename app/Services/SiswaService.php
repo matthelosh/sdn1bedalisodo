a29f5de31aa6b90774a4dd1c881833931e260ad3
Muhammad Soleh <matthelosh@gmail.com>
2023-08-20 08:47:00 +0700

    Add foto siswa

---
+++ app/Services/SiswaService.php
@@ -0,0 +1,63 @@
+<?php
+namespace App\Services;
+
+use App\Models\Siswa;
+use Illuminate\Support\Facades\Storage;
+
+class SiswaService
+{
+    public function store($siswa, $foto=null)
+    {
+        try {
+
+            if ($foto !== null) {
+                $foto_url = $this->storeFoto($foto, $siswa->nisn);
+            }
+
+            $store = Siswa::updateOrCreate(
+                [
+                    'id' => $siswa->id ?? null,
+                ],
+                [
+                    'nisn' => $siswa->nisn,
+                    'nis' => $siswa->nis ?? null,
+                    'nama' => $siswa->nama,
+                    'jk' => $siswa->jk,
+                    'tempat_lahir' => $siswa->tempat_lahir,
+                    'tanggal_lahir' => $siswa->tanggal_lahir,
+                    'agama' => $siswa->agama,
+                    'alamat' => $siswa->alamat,
+                    'rt' => $siswa->rt ?? null,
+                    'rw' => $siswa->rw ?? null,
+                    'desa' => $siswa->desa,
+                    'kecamatan' => $siswa->kecamatan,
+                    'kode_pos' => $siswa->kode_pos,
+                    'kabupaten' => $siswa->kabupaten,
+                    'angkatan' => $siswa->angkatan,
+                    'hp' => $siswa->hp ?? null,
+                    'email' => $siswa->email ?? null,
+                    'nik' => $siswa->nik ?? null,
+                    'is_active' => $siswa->is_active ?? '1',
+                    'status' => $siswa->status ?? 'aktif',
+                    'foto_url' => $foto !== null ? $foto_url : $siswa->foto_url
+                ]
+            );
+
+            return $store;
+        } catch(\Exception $e)
+        {
+
+        }
+    }
+
+    public function storeFoto($foto, $nisn) {
+        try {
+            
+            $store = $foto->storePubliclyAs('images/siswas', $nisn.'.'.$foto->extension(), 's3');
+            return Storage::disk('s3')->url($store);
+        } catch(\Exception $e)
+        {
+
+        }
+    }
+}
