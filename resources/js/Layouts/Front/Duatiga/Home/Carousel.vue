a38365807f103a7ad42c50afdf4c093a881b02c1
Muhammad Soleh <matthelosh@gmail.com>
2023-10-05 15:03:40 +0700

    Add default Image to Carousel

--- resources/js/Layouts/Front/Duatiga/Home/Carousel.vue
+++ resources/js/Layouts/Front/Duatiga/Home/Carousel.vue
@@ -6,6 +6,9 @@ import '@splidejs/vue-splide/css';
 
 const props = defineProps({items: Array})
 
+const setDefaultImage = (e) => {
+    e.target.src = '/img/kegiatan/8.jpg'
+}
 </script>
 
 <template>
@@ -15,7 +18,7 @@ const props = defineProps({items: Array})
         class="group"
         style="background-color: #e12525;"
     >
-      <img :src="item.featured_image" :alt="item.title" class="w-full filter grayscale opacity-50 hover:opacity-100 transition-all hover:grayscale-0">
+      <img :src="item.featured_image" :alt="item.title" class="w-full filter grayscale opacity-50 hover:opacity-100 transition-all hover:grayscale-0" @error="setDefaultImage">
       <Link :href="`/${item.category.label.toLowerCase()}/${item.slug}`" class="absolute bottom-10 left-[50%] -translate-x-[50%]  group-hover:bg-slate-800 bg-slate-200 group-hover:text-white p-4 text-xl transition-all  hover:shadow-lg rounded-lg">{{ item.title }}</Link>
     </SplideSlide>
   </Splide>
